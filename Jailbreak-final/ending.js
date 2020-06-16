041ceca99acc08644d715e6737bc11c19ce77248
HSun666 <64248645+HSun666@users.noreply.github.com>
2020-06-16 11:34:09 -0700

    Add files via upload

---
+++ Jailbreak-final/ending.js
@@ -0,0 +1,6 @@
+document.getElementById("display").innerHTML = "Very good! You have sucessfully made a jailbreak! <br><br><br> If you get more than 10 coins, then you have mastered this game. <br><br><br> If you get less coins, then I strongly recommend you to replay this game. <br><br><br> Possibly one day, you are going to make a real jailbreak in your real life. <br><br><br><br><br> Brought to you by: Haibo. Great thanks to Ms.Wear.";
+
+
+function button(){
+	window.location.href = "start.html";
+}
